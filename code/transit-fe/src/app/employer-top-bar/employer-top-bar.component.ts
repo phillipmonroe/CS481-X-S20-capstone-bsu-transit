20f4f8d165458de10ea5999f7ce1adbc685940c4
Phillip Monroe <monroephillip@gmail.com>
2020-02-25 14:09:40 -0700

    Implements #10

---
+++ code/transit-fe/src/app/employer-top-bar/employer-top-bar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-employer-top-bar',
+  templateUrl: './employer-top-bar.component.html',
+  styleUrls: ['./employer-top-bar.component.css']
+})
+export class EmployerTopBarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
