ad81465c5694f4af7b8e582eefa4e7aeb751c546
Phillip Monroe <monroephillip@gmail.com>
2020-02-27 13:57:20 -0700

    Added Employer Routing

--- code/transit-fe/src/app/app-routing.module.ts
+++ code/transit-fe/src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { CityGoAdminComponent } from './city-go-admin/city-go-admin.component';
+import { EmployerComponent } from './employer/employer.component';
 
 /**
  * --Add Component Paths Here--
  */
 const routes: Routes = [
   {path: 'city-go-admin', component: CityGoAdminComponent },
+  {path: 'employer', component: EmployerComponent }
 ];
 
 @NgModule({
