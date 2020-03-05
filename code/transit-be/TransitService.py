0a5b4e0a539593e13e7cf20d20350a21e9827385
Garett Skaar <skaargarett@gmail.com>
2020-03-05 06:18:51 -0700

    References #18, adds endpoint prototypes and seperate function file

--- code/transit-be/TransitService.py
+++ code/transit-be/TransitService.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, Response
+from TransitServiceFunctions import *
 import mysql.connector
 
 app = Flask(__name__)
@@ -9,6 +10,28 @@ def test_service():
     Test service is running correctly. This route exposes an endpoint that returns a service is running prompt.
     """
     return "Transit backend service is running."
+
+# GET returns all employers
+# POST creates a new employer with the request body data
+@app.route('/employers', methods=['GET', 'POST'])
+def employers():
+    if request.method == 'GET':
+        return getEmployers()
+    elif request.method == 'POST':
+        return createEmployer()
+
+# GET returns employer with id == {id}
+# PUT updates employer with id == {id}
+# DELETE removes employer with id == {id}
+@app.route('/employers/{id}', methods=['GET', 'PUT', 'DELETE'])
+def employersId():
+    if request.method == 'GET':
+        return getEmployer()
+    elif request.method == 'PUT':
+        return updateEmployer()
+    elif request.method == 'DELETE':
+        return deleteEmployer()
+
 
 # For development only, do not deploy into production.
 if __name__ == '__main__':
---
+++ code/transit-be/TransitServiceFunctions.py
@@ -0,0 +1,17 @@
+# Transit Service Functions
+
+# Return all Employers
+def getEmployers():
+    return "Reached all Employers"
+# Add new Employer
+def createEmployer():
+    return "Reached create employer"
+# Get specified Employer
+def getEmployer():
+    return "Reached get Employer"
+# Update specified Employer
+def updateEmployer():
+    return "Reached update Employer"
+# Delete specified Employer
+def deleteEmployer():
+    return "Reached delete Employer"
