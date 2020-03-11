5f49926206ff7dedc5da09da95cd748c93af2cc4
Phillip Monroe <monroephillip@gmail.com>
2020-03-11 16:56:26 -0600

    Added flask marshmallow for help returning JSON

--- code/transit-be/app/__init__.py
+++ code/transit-be/app/__init__.py
@@ -1,13 +1,16 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
 
 db = SQLAlchemy()
+ma = Marshmallow()
 
 def create_app():
     """Construct the core application."""
     app = Flask(__name__, instance_relative_config=False)
     app.config.from_object('config.Config')
     db.init_app(app)
+    ma.init_app(app)
 
     with app.app_context():
         from . import routes
