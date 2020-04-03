c05b18c606c7e56d702615ca4429bcae7410f1ee
Desmond Porth <dporth000@gmail.com>
2020-04-03 14:26:00 -0600

    Added fake data for city-go-admin

--- code/transit-fe/src/app/city-go-admin/shared/employer.service.ts
+++ code/transit-fe/src/app/city-go-admin/shared/employer.service.ts
@@ -1,18 +1,19 @@
 import { Injectable } from '@angular/core';
 import { Employer } from './employer.model';
-import { Subject, Observable } from 'rxjs';
-
+import { Subject } from 'rxjs';
+import { EMPLOYERS } from './mock-employers';
 @Injectable()
 export class EmployerService {
   
   private employers = new Subject<Employer[]>();
   employers$ = this.employers.asObservable();
 
+  initEmployers() {
+    this.employers.next(EMPLOYERS);
+    this.employers$ = this.employers.asObservable();
+  }
+  
   addEmployer(employer: Employer[]) {
     this.employers.next(employer);
-  }
-
-  public getEmployersShared(): Observable<Employer[]> {
-    return this.employers.asObservable();
   }
 }
